aa905fb02e3606cf468307a46ef7fa485e8d5105
Lem Redd <lmoa.jhdp@gmail.com>
2024-09-21 14:47:19 +0800

    feat(action): validate only

--- actions/register.ts
+++ actions/register.ts
@@ -2,8 +2,6 @@
 
 import { RegisterSchema } from '@/schemas'
 import * as z from 'zod'
-import { createClient } from '@/utils/supabase/server'
-import { SignUpWithPasswordCredentials } from '@supabase/supabase-js'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -11,23 +9,5 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return {
       error: 'Invalid fields!',
     }
-  }
-  const options: SignUpWithPasswordCredentials["options"] = {
-    "data": {
-      "first_name": values.firstName,
-      "last_name": values.lastName
-    }
-  }
-
-  const supabase = createClient()
-  const { error } = await supabase.auth.signUp({
-    "email": values.email,
-    "password": values.password,
-    options
-  })
-
-  if (error) return { error: error.code }
-  return {
-    success: 'Email sent!',
   }
 }
