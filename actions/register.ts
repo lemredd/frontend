e712b0a5d5b1599645380a39ef881ce25bbb566e
Lem Redd <lmoa.jhdp@gmail.com>
2024-09-21 14:16:38 +0800

    feat(action): store registration metadata

--- actions/register.ts
+++ actions/register.ts
@@ -3,6 +3,7 @@
 import { RegisterSchema } from '@/schemas'
 import * as z from 'zod'
 import { createClient } from '@/utils/supabase/server'
+import { SignUpWithPasswordCredentials } from '@supabase/supabase-js'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -11,11 +12,21 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
       error: 'Invalid fields!',
     }
   }
+  const options: SignUpWithPasswordCredentials["options"] = {
+    "data": {
+      "first_name": values.firstName,
+      "last_name": values.lastName
+    }
+  }
 
   const supabase = createClient()
-  const { error } = await supabase.auth.signUp({ "email": values.email, "password": values.password })
+  const { error } = await supabase.auth.signUp({
+    "email": values.email,
+    "password": values.password,
+    options
+  })
 
-  if (error) return { error: error.code, }
+  if (error) return { error: error.code }
   return {
     success: 'Email sent!',
   }
