020cb76db72e52a245787b2564a868f543ca1a3e
lemredd <lmoa@jhdp@gmail.com>
2024-09-22 23:16:08 +0800

    feat(action): update profile descriptions

---
+++ actions/description.ts
@@ -0,0 +1,38 @@
+'use server'
+
+import * as z from 'zod'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+
+import { DescriptionSchema } from '@/schemas'
+import { createClient } from '@/utils/supabase/server'
+
+export const describe = async (values: z.infer<typeof DescriptionSchema>) => {
+  const supabase = createClient()
+  const validatedFields = DescriptionSchema.safeParse(values)
+
+  if (!validatedFields.success) {
+    return {
+      error: 'Invalid fields!',
+    }
+  }
+
+  const { data: { user } } = await supabase.auth.getUser()
+  const { data: profile } = await supabase
+    .from('profiles')
+    .select('id')
+    .eq('user_id', user!.id)
+    .single()
+
+  const { error } = await supabase
+    .from('profiles')
+    .update({
+      short_desc: values.shortDescription,
+      long_desc: values.longDescription
+    })
+    .eq('id', profile!.id)
+  if (error) return { error: error.message }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
--- actions/skills.ts
+++ actions/skills.ts
@@ -19,20 +19,11 @@ export const addSkills = async (values: z.infer<typeof SkillsSchema>) => {
   const { data: { user } } = await supabase.auth.getUser()
   const { data: profile } = await supabase.from('profiles').select('id').eq('user_id', user!.id).single()
 
-  let { error } = await supabase
+  const { error } = await supabase
     .from('profile_skills')
     .insert(validatedFields.data.skillIds.map((id) => ({ profile_id: profile!.id, skill_id: id })))
-  console.error("add profile skills", error)
-  if (error) return { error: error.code }
-
-  // TODO: redirect to `/user/setup/description`
-
-  error = (
-    await supabase.from('profiles').update({ is_completed: true }).eq('id', profile!.id)
-  ).error
-  console.error("is completed update", error)
   if (error) return { error: error.code }
 
   revalidatePath('/', 'layout')
-  redirect('/')
+  redirect('/user/setup/description')
 }
