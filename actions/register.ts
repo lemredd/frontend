2b0814e05024aa80402ffdfaebd9322976275f72
lemredd <lmoa@jhdp@gmail.com>
2024-09-22 13:49:29 +0800

    feat(action): sign up user

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { RegisterSchema, RegisterWithRoleSchema } from '@/schemas'
+import { createClient } from '@/utils/supabase/server'
 import * as z from 'zod'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -13,11 +14,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 }
 
 export const registerWithRole = async (values: z.infer<typeof RegisterWithRoleSchema>) => {
-  console.log("register with role",values)
   const validatedFields = RegisterWithRoleSchema.safeParse(values)
   if (!validatedFields.success) {
     return {
       error: 'Invalid fields!',
     }
   }
+
+  const supabase = createClient()
+
+  const { error } = await supabase.auth.signUp({
+    email: values.email,
+    password: values.password,
+    options: {
+      data: {
+        first_name: values.firstName,
+        last_name: values.lastName,
+        role_code: values.roleCode,
+      },
+    },
+  })
+
+  if (error) return { error: error.message }
 }
