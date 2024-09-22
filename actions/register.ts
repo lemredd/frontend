a2d48c641269ee9d1665ae6fcb45603e91ff1d85
lemredd <lmoa@jhdp@gmail.com>
2024-09-22 11:41:57 +0800

    feat(action): register with role

--- actions/register.ts
+++ actions/register.ts
@@ -1,10 +1,20 @@
 'use server'
 
-import { RegisterSchema } from '@/schemas'
+import { RegisterSchema, RegisterWithRoleSchema } from '@/schemas'
 import * as z from 'zod'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
+  if (!validatedFields.success) {
+    return {
+      error: 'Invalid fields!',
+    }
+  }
+}
+
+export const registerWithRole = async (values: z.infer<typeof RegisterWithRoleSchema>) => {
+  console.log("register with role",values)
+  const validatedFields = RegisterWithRoleSchema.safeParse(values)
   if (!validatedFields.success) {
     return {
       error: 'Invalid fields!',
