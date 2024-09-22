9f6c233c57c05b40a9bfe4178f67834b6c090ea3
lemredd <lmoa@jhdp@gmail.com>
2024-09-22 11:41:18 +0800

    feat(schema): make `RegisterWithRoleSchema`

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,7 @@
 import * as z from 'zod'
 
+import { createClient } from '@/utils/supabase/client'
+
 export const LoginSchema = z.object({
   email: z.string().email({
     message: 'Email is required',
@@ -9,53 +11,54 @@ export const LoginSchema = z.object({
   }),
 })
 
+const registerFields = {
+  email: z.string().email({
+    message: 'Email is required',
+  }),
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .regex(/\d/, 'Password must contain at least one digit')
+    .regex(
+      /[^A-Za-z0-9]/,
+      'Password must contain at least one special character',
+    ),
+  confirmPassword: z
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .regex(/\d/, 'Password must contain at least one digit')
+    .regex(
+      /[^A-Za-z0-9]/,
+      'Password must contain at least one special character',
+    ),
+  firstName: z.string().min(1, {
+    message: 'Name is required',
+  }),
+  lastName: z.string().min(1, {
+    message: 'Name is required',
+  }),
+  // }),
+  // profile_picture:
+  //   typeof window === 'undefined'
+  //     ? z.any()
+  //     : z.instanceof(FileList).optional(),
+  // province: z.string().optional(),
+  // city: z.string().optional(),
+  // barangay: z.string().optional(),
+  terms: z
+    .boolean()
+    .refine(
+      (value) => value === true,
+      'You must agree to the terms and conditions',
+    ),
+}
 export const RegisterSchema = z
   .object({
-    email: z.string().email({
-      message: 'Email is required',
-    }),
-    password: z
-      .string()
-      .min(8, 'Password must be at least 8 characters')
-      .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-      .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-      .regex(/\d/, 'Password must contain at least one digit')
-      .regex(
-        /[^A-Za-z0-9]/,
-        'Password must contain at least one special character',
-      ),
-    confirmPassword: z
-      .string()
-      .min(8, 'Password must be at least 8 characters')
-      .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-      .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-      .regex(/\d/, 'Password must contain at least one digit')
-      .regex(
-        /[^A-Za-z0-9]/,
-        'Password must contain at least one special character',
-      ),
-    firstName: z.string().min(1, {
-      message: 'Name is required',
-    }),
-    lastName: z.string().min(1, {
-      message: 'Name is required',
-    }),
-    // role: z.string().min(1, {
-    //   message: 'Role is required',
-    // }),
-    // profile_picture:
-    //   typeof window === 'undefined'
-    //     ? z.any()
-    //     : z.instanceof(FileList).optional(),
-    // province: z.string().optional(),
-    // city: z.string().optional(),
-    // barangay: z.string().optional(),
-    terms: z
-      .boolean()
-      .refine(
-        (value) => value === true,
-        'You must agree to the terms and conditions',
-      ),
+    ...registerFields
   })
   .superRefine(({ confirmPassword, password }, ctx) => {
     if (confirmPassword !== password) {
@@ -66,3 +69,18 @@ export const RegisterSchema = z
       })
     }
   })
+
+export const RegisterWithRoleSchema = z.object({
+  ...registerFields,
+  roleCode: z.string().min(1, {
+    message: 'role is required',
+  }).refine(async value => {
+    const client = createClient()
+    const { error } = await client
+      .from('roles')
+      .select('code')
+      .eq('code', value)
+      .single()
+    return !error
+  }, 'Role does not exist')
+})
