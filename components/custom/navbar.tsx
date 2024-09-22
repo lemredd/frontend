417d6b20100479038bbe8417bf42cecb4e00fb53
lemredd <lmoa@jhdp@gmail.com>
2024-09-22 17:28:49 +0800

    feat(component): pass user

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -11,12 +11,18 @@ import {
 import useActivePath from '@/hooks/useActivePath'
 import useNavbarRoutes from '@/hooks/useNavbarRoutes'
 import { cn } from '@/lib/utils'
+import { User } from '@supabase/supabase-js'
 import { Menu } from 'lucide-react'
 import Link from 'next/link'
 import { RiUserSearchFill } from 'react-icons/ri'
-export function Navbar() {
+
+interface NavbarProps {
+  user: User | null
+}
+export function Navbar({ user }: NavbarProps) {
   const navbarRoutes = useNavbarRoutes()
   const activePath = useActivePath()
+  console.log("navbar", user)
 
   return (
     <nav className="fixed top-0 z-50 bg-background w-full shadow-sm py-5 md:py-0">
@@ -121,15 +127,65 @@ export function Navbar() {
         </div>
 
         <div className="md:flex hidden md:items-center md:justify-center space-x-2 md:space-x-1 mt-2 md:pb-0 md:mt-0">
-          <Button
-            variant="ghost"
-            asChild
-          >
-            <Link href="/auth/login">Login</Link>
-          </Button>
-          <Button asChild>
-            <Link href="/auth/join">Join Now</Link>
-          </Button>
+          {!user ? (
+            <>
+              <Button
+                variant="ghost"
+                asChild
+              >
+                <Link href="/auth/login">Login</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/auth/join">Join Now</Link>
+              </Button>
+            </>
+          ) : (
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="outline">
+                  {' '}
+                  <Menu />
+                </Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle className="inline-flex space-x-1 items-center">
+                    <RiUserSearchFill className="size-6 fill-primary" />
+                    <h1 className="text-xl font-bold text-primary">
+                      Task Grabber
+                    </h1>
+                  </SheetTitle>
+                </SheetHeader>
+
+                <ul className="justify-center items-center space-y-5 mt-8 md:flex md:space-x-6 md:space-y-0">
+                  {navbarRoutes.map((route, idx) => (
+                    <li
+                      key={idx}
+                      className={cn(
+                        'text-gray-600 hover:text-primary relative md:py-5',
+                        activePath === route.path ||
+                          (route.path !== '/' &&
+                            activePath.startsWith(route.path))
+                          ? 'text-primary'
+                          : '',
+                      )}
+                    >
+                      <Link href={route.path}>{route.label}</Link>
+                    </li>
+                  ))}
+                  <li className="w-fit mx-auto">
+                    <form
+                      action="/auth/logout"
+                      method="post"
+                      className="w-fit"
+                    >
+                      <Button variant="destructive">Logout</Button>
+                    </form>
+                  </li>
+                </ul>
+              </SheetContent>
+            </Sheet>
+          )}
         </div>
       </div>
     </nav>
