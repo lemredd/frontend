aae26bb4112c84d44cb20adf6938c1bcdec24a65
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2024-09-24 00:46:44 +0800

    chore(routes): add initial admin dashboard route

---
+++ app/(private)/admin/dashboard/page.tsx
@@ -0,0 +1,13 @@
+// import Image from 'next/image'
+export default function Dashboard() {
+  return (
+    <main className="h-screen w-full relative ">
+      {/* <Image
+        className="object-top object-cover"
+        src="/images/ideas.jpg"
+        alt="asd"
+        fill
+      /> */}
+    </main>
+  )
+}
