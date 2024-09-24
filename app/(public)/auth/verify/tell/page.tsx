115fe2567cbb0366af4caefe7ad5442cd1f41b6e
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2024-09-24 13:40:57 +0800

    chore(verify): modify verify page

--- app/(public)/auth/verify/tell/page.tsx
+++ app/(public)/auth/verify/tell/page.tsx
@@ -1,16 +1,30 @@
-"use client"
+'use client'
 
-import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Mail } from 'lucide-react'
 
 export default function VerifyPage() {
   return (
-    <main className="flex h-full flex-col items-center justify-center">
-      <Card>
-        <CardHeader>
-          <h2 className="text-lg font-semibold">Email Verification sent</h2>
+    <main className="flex h-full flex-col items-center justify-center p-4 d">
+      <Card className="shadow-lg max-w-md w-full rounded-lg p-6 bg-white border border-gray-200">
+        <CardHeader className="flex flex-col items-center">
+          <Mail className="w-16 h-16 text-primary mb-4" />
+          <h2 className="text-2xl font-bold text-gray-800">
+            Verification Email Sent!
+          </h2>
         </CardHeader>
-        <CardContent>
-          <p>Check your email for verification link</p>
+        <CardContent className="text-center">
+          <p className="text-gray-600 mb-4">
+            Please check your inbox for a verification link to complete your
+            sign-up.
+          </p>
+          <p className="text-sm text-gray-500">
+            If you haven't received the email, check your spam folder or{' '}
+            <span className="text-blue-500">resend</span> the email.
+          </p>
+          {/* ADD RESEND */}
+          <Button className="mt-6 w-full">Resend Verification Email</Button>
         </CardContent>
       </Card>
     </main>
