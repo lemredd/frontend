f04384c1fd14529675aba8be7a9f97e997bd21f1
lemredd <lmoa@jhdp@gmail.com>
2024-09-24 23:31:11 +0800

    feat(action): post job and mark onboarding complete

--- actions/pdr/job.ts
+++ actions/pdr/job.ts
@@ -1,10 +1,12 @@
+"use server"
+
 import { z } from 'zod'
 import { redirect } from 'next/navigation'
 
 import { JobSchema } from '@/schemas'
 import { createClient } from '@/utils/supabase/server'
 
-export async function postJob(values: z.infer<typeof JobSchema>) {
+export async function postJob(values: z.infer<typeof JobSchema>, isOnboarding: boolean) {
   const validatedFields = JobSchema.safeParse(values)
   if (!validatedFields.success) {
     return {
@@ -13,14 +15,47 @@ export async function postJob(values: z.infer<typeof JobSchema>) {
   }
 
   const supabase = createClient()
-  const { error } = await supabase
-    .from('jobs')
-    .insert([
-      {
-        ...validatedFields.data,
-      }
-    ])
 
+  const { data: { user } } = await supabase.auth.getUser()
+  const { data: profile } = await supabase
+    .from('profiles')
+    .select<string, { id: string }>('id')
+    .eq('user_id', user!.id)
+    .single()
+
+  const { data: job, error } = await supabase
+    .from('jobs')
+    .insert({
+      name: validatedFields.data.name,
+      description: validatedFields.data.description,
+      price: validatedFields.data.price,
+      province: validatedFields.data.province,
+      city_muni: validatedFields.data.city_muni,
+      barangay: validatedFields.data.barangay,
+      profile_id: profile!.id,
+    })
+    .select()
   if (error) return { error: error.message }
-  redirect('/')
+
+  const jobSkills = validatedFields.data.skill_ids.map(id => ({
+    job_id: job![0].id,
+    skill_id: id,
+  }))
+  const { error: jobSkillsError } = await supabase
+    .from('job_skills')
+    .insert(jobSkills)
+
+  if (jobSkillsError) return { error: jobSkillsError.message }
+
+  if (isOnboarding) {
+    const { error: onboardingError } = await supabase
+      .from('profiles')
+      .update({ is_completed: true })
+      .eq('id', profile!.id)
+
+    if (onboardingError) return { error: onboardingError.message }
+    redirect('/')
+  }
+
+  return { success: "Job posted" }
 }
