b553a0a6ea1e54877d58a46f9dcf281c3bc4327b
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2024-09-27 00:12:56 +0800

    feat: add top loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { createClient } from '@/utils/supabase/server'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import NextTopLoader from 'nextjs-toploader'
 import './globals.css'
 
 const poppins = Poppins({
@@ -38,6 +39,21 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NextTopLoader
+            color="#2299DD"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+            template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+            zIndex={1600}
+            showAtBottom={false}
+          />
           <Navbar user={user} />
           {children}
           <Footer role={user?.user_metadata?.role_code} />
