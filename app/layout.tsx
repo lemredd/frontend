9d8693bbe55c6b527ee545ba455440cbee2955e9
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2024-09-26 15:37:53 +0800

    chore: add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+import Footer from '@/components/custom/footer'
 import { Navbar } from '@/components/custom/navbar'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { createClient } from '@/utils/supabase/server'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
-import { createClient } from '@/utils/supabase/server'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -20,9 +21,10 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-
   const supabase = createClient()
-  const { data: { user } } = await supabase.auth.getUser()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
 
   return (
     <html
@@ -37,9 +39,8 @@ export default async function RootLayout({
           disableTransitionOnChange
         >
           <Navbar user={user} />
-          <main className="w-full 2xl:container h-screen mx-auto pt-20 md:pt-16">
-            {children}
-          </main>
+          {children}
+          <Footer role={user?.user_metadata?.role_code} />
         </ThemeProvider>
       </body>
     </html>
