44a85211bf53ab2ff25503d6a42e9bfc8dbdfb59
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2024-09-28 18:45:00 +0800

    chore(chip): adjust chip design

--- components/ui/chip.tsx
+++ components/ui/chip.tsx
@@ -4,7 +4,10 @@ interface ChipProps {
 }
 export function Chip({ content, afterContent }: ChipProps) {
   return (
-    <div className="rounded-full p-2 flex items-center gap-2 border border-slate-300" title={content}>
+    <div
+      className="rounded-full px-3 py-1 flex items-center gap-2 border border-slate-300"
+      title={content}
+    >
       <span className="text-sm max-w-[100px] truncate">{content}</span>
       {afterContent}
     </div>
