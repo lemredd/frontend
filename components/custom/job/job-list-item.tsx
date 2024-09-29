7b05fcb03c741fc787cde806acb6fbdf35ea14d1
lemredd <lmoa@jhdp@gmail.com>
2024-09-29 21:13:24 +0800

    feat(component): show job address

--- components/custom/job/job-list-item.tsx
+++ components/custom/job/job-list-item.tsx
@@ -1,6 +1,9 @@
-import { Button } from "@/components/ui/button"
-import { getRecency } from "@/lib/utils"
 import { useState } from "react"
+import { MapPin } from "lucide-react"
+
+import { getRecency } from "@/lib/utils"
+import { Chip } from "@/components/ui/chip"
+import { Button } from "@/components/ui/button"
 
 interface Props {
   job: Record<string, string>
@@ -16,9 +19,24 @@ export default function JobListItem({ job }: Props) {
     `
   )
 
+  function getAddress({ province, city_muni, barangay }: Props["job"]) {
+    let address = ""
+    if (barangay) address += `${barangay}, `
+    if (city_muni) address += `${city_muni}, `
+    if (province) address += `${province}`
+
+    return address
+  }
+
   return (
     <div className="border p-4 rounded space-y-2">
       <a href={`${job.id}`}><h3 className="text-lg font-semibold">{job.name}</h3></a>
+      <Chip
+        beforeContent={<MapPin size={16} />}
+        content={getAddress(job)}
+        className="w-max"
+        contentClassName="max-w-[unset]"
+      />
       <div>
         <p className={descriptionClassName()}>{job.description}</p>
         <Button variant="link" className="p-0 -mt-2" onClick={() => setCollapsed(!collapsed)}>See {collapsed ? 'more' : 'less'}</Button>
--- components/custom/job/job-list.tsx
+++ components/custom/job/job-list.tsx
@@ -1,51 +1,56 @@
-'use client'
+"use client"
 
-import { createClient } from '@/utils/supabase/client'
-import { useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
-import JobListItem from './job-list-item'
+import { useAuthStore } from "@/store/AuthStore";
+import { createClient } from "@/utils/supabase/client";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import JobListItem from "./job-list-item";
 
 export default function JobList() {
+  const { user, profile } = useAuthStore()
   const searchParams = useSearchParams()
   const supabase = createClient()
   const [jobs, setJobs] = useState<Record<string, string>[]>([])
 
   useEffect(() => {
-    const page = +(searchParams.get('page') ?? 1)
-    const size = +(searchParams.get('size') ?? 10)
+    if (!user || !profile) return
+    const page = +(searchParams.get("page") ?? 1)
+    const size = +(searchParams.get("size") ?? 10)
     const start = (page - 1) * size
     const end = start + size
 
+    const selectedFields = "id, created_at, name, description, price, province, city_muni, barangay"
     const query = supabase
-      .from('jobs')
-      .select('id, created_at, name, description, price')
-      .order('created_at', { ascending: false })
+      .from("jobs")
+      .select(selectedFields)
+      .order("created_at", { ascending: false })
       .range(start, end)
 
-    const search = searchParams.get('search')
-    if (search) query.textSearch('search_jobs', `'${search}'`)
+    const { role_code: roleCode } = user.user_metadata
+    if (roleCode === "PDR") query
+      .eq("profile_id", profile.id)
+
+    const search = searchParams.get("search")
+    if (search) query
+      .textSearch('search_jobs', `'${search}'`)
 
     // TODO: default query to user province
-    const location = searchParams.get('location')
-    if (location)
-      query.or(
-        `province.ilike.%${location}%,city_muni.ilike.%${location}%,barangay.ilike.%${location}%`,
-      )
+    const location = searchParams.get("location")
+    if (location) query
+      .or(`province.ilike.%${location}%,city_muni.ilike.%${location}%,barangay.ilike.%${location}%`)
 
-    query.then(({ data, error }) => {
-      if (data) setJobs(data)
-      if (error) console.error(error)
-    })
+    query
+      .then(({ data, error }) => {
+        if (data) setJobs(data)
+        if (error) console.error(error)
+      })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchParams])
 
   return (
     <div className="space-y-4">
       {jobs.map((job) => (
-        <JobListItem
-          key={job.id}
-          job={job}
-        />
+        <JobListItem key={job.id} job={job} />
       ))}
     </div>
   )
