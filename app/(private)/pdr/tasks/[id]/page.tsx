df9d86a37299fdaf6e1bf77e6c00298f048b3fb1
lemredd <lmoa@jhdp@gmail.com>
2024-10-01 22:17:15 +0800

    feat(page): show job details form

--- app/(private)/pdr/tasks/[id]/page.tsx
+++ app/(private)/pdr/tasks/[id]/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { createClient } from "@/utils/supabase/client"
 import { useAuthStore } from "@/store/AuthStore"
 import { ProviderJobDetailsHeader } from "@/components/custom/job/job-details-header"
+import { JobDetailsForm } from "@/components/custom/job/job-details-form"
 
 interface Props {
   params: {
@@ -15,24 +16,30 @@ export default function ProviderTaskPage({ params: { id } }: Props) {
   const supabase = createClient()
   const { profile } = useAuthStore()
   const [job, setJob] = useState<Record<string, unknown>>()
-  const isOwned = profile?.id === job?.profile_id
+  const isOwned = !!profile && !!job && (profile?.id === job?.profile_id)
 
   useEffect(() => {
     supabase
       .from("jobs")
-      .select()
+      .select<string, typeof job>(`
+        id, name, description, price, status, profile_id,
+        job_skills (
+          skills (id, name)
+        )
+      `)
       .match({ id })
       .single()
       .then(({ data, error }) => {
         if (error) console.error(error)
-        setJob(data)
+        setJob(data!)
       })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <section>
+    <section className="space-y-4">
       <ProviderJobDetailsHeader isOwned={isOwned} job={job} />
+      <JobDetailsForm isOwned={isOwned} job={job} />
     </section>
   )
 }
