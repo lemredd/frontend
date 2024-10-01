77db434182cd41eb074b5eed0762f7bf287b9bc0
lemredd <lmoa@jhdp@gmail.com>
2024-10-01 21:44:29 +0800

    feat(component): make provider job details header

--- components/custom/job/job-details-header.tsx
+++ components/custom/job/job-details-header.tsx
@@ -6,22 +6,71 @@ import { Button } from "@/components/ui/button"
 import { useEffect, useState, useTransition } from "react"
 import { applyJob, checkJobApplication } from "@/actions/pdr/job"
 import { useAuthStore } from "@/store/AuthStore"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Link from "next/link"
+import { Chip } from "@/components/ui/chip"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu"
 
 interface Props {
-  job: Record<string, unknown>
+  job?: Record<string, unknown>
+}
+
+interface ProviderJobDetailsHeaderProps extends Props {
+  isOwned: boolean
+}
+
+export function ProviderJobDetailsHeader({ isOwned, job }: ProviderJobDetailsHeaderProps) {
+  const TABS = ["details", "applicants"]
+  return (
+    <header className="grid grid-flow-row gap-4 grid-cols-[1fr_auto] grid-rows-2">
+      <h1 className="flex items-center gap-x-2 text-2xl font-semibold capitalize">
+        {job?.name as string}
+        <Chip content={job?.status as string} className="w-max" />
+      </h1>
+      <h3 className="text-lg self-center">₱{job?.price as string} PHP</h3>
+      <div className="flex gap-x-4">
+        <Tabs defaultValue={TABS[0]}>
+          <TabsList>
+            {TABS.map(tab => (
+              <TabsTrigger key={tab} value={tab} asChild>
+                <Link href={tab === "details" ? "./" : "./applicants"}>
+                  <span className="capitalize">{tab}</span>
+                </Link>
+              </TabsTrigger>
+            ))}
+
+          </TabsList>
+        </Tabs>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary">...</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup className="px-2 py-1">
+              {isOwned && (
+                <DropdownMenuItem className="hover:outline-none px-2 hover:bg-rose-500 transition-colors rounded cursor-pointer">
+                  Delete
+                </DropdownMenuItem>
+              )}
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  )
 }
 
 export default function JobDetailsHeader({ job }: Props) {
   const { user } = useAuthStore()
   const [isPending, startTransition] = useTransition()
   const [application, setApplication] = useState<Record<string, unknown>>()
-  console.log(application)
 
   function apply() {
     if (!user) return
     startTransition(() => {
       applyJob({
-        job_id: job.id as string,
+        job_id: job?.id as string,
         user_id: user.id
       }).then(({ error, application }) => {
         if (error) return console.error(error)
@@ -34,19 +83,19 @@ export default function JobDetailsHeader({ job }: Props) {
     if (!user) return
     startTransition(() => {
       checkJobApplication({
-        job_id: job.id as string,
+        job_id: job?.id as string,
         user_id: user.id
       }).then(({ error, application }) => {
         if (error) return console.error(error)
         if (application) setApplication(application)
       })
     })
-  }, [user, job.id])
+  }, [user, job?.id])
 
   return (
     <header className="grid grid-flow-col grid-cols-2 grid-rows-2">
-      <h1 className="text-2xl font-semibold capitalize">{job.name as string}</h1>
-      <h3 className="text-lg">₱{job.price as string} PHP</h3>
+      <h1 className="text-2xl font-semibold capitalize">{job?.name as string}</h1>
+      <h3 className="text-lg">₱{job?.price as string} PHP</h3>
       <div className="row-span-2 self-center items-center justify-end flex gap-x-2">
         {!application && (
           <Button onClick={apply} disabled={isPending}>{isPending ? 'Please wait...' : 'Apply'}</Button>
