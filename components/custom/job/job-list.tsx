8a25e0ed52ee5b0e532159c84b73838484d96425
lemredd <lmoa@jhdp@gmail.com>
2024-10-03 22:03:24 +0800

    refactor(hook): use pagination search params

--- components/custom/job/job-list.tsx
+++ components/custom/job/job-list.tsx
@@ -14,6 +14,7 @@ export default function JobList() {
 
   useEffect(() => {
     if (!user || !profile) return
+    // TODO: usePaginationSearchParams
     const page = +(searchParams.get("page") ?? 1)
     const size = +(searchParams.get("size") ?? 10)
     const start = (page - 1) * size
