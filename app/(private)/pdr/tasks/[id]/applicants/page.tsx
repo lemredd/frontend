c6f311dfd2dbd24d45e0984de6e128cb983e66b1
lemredd <lmoa@jhdp@gmail.com>
2024-10-03 22:03:49 +0800

    feat(page): prepare listing job applicants

--- app/(private)/pdr/tasks/[id]/applicants/page.tsx
+++ app/(private)/pdr/tasks/[id]/applicants/page.tsx
@@ -1,9 +1,53 @@
 "use client"
 
-export default function TaskApplicantsPage() {
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import usePaginationSearchParams from "@/hooks/usePaginationSearchParams"
+import { createClient } from "@/utils/supabase/client"
+import { useEffect, useState } from "react"
+
+interface Props {
+  params: {
+    id: string
+  }
+}
+export default function TaskApplicantsPage({ params: { id } }: Props) {
+  const supabase = createClient()
+  const { start, end } = usePaginationSearchParams()
+  const [applicants, setApplicants] = useState<Record<string, unknown>[]>([])
+  const [totalApplicants, setTotalApplicants] = useState(0)
+
+  useEffect(() => {
+    const fields = "*, profiles (id, first_name, last_name, profile_skills(skills(id,name)))"
+    supabase
+      .from("job_applicants")
+      .select<string, typeof applicants[number]>(fields, { count: "exact" })
+      .eq("job_id", id)
+      .order("created_at", { ascending: false })
+      .range(start, end)
+      .then(({ data, error, count }) => {
+        if (data) setApplicants(data)
+        if (count) setTotalApplicants(count)
+        if (error) console.error(error)
+      })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <div>
-      <h2 className="text-xl">Task Applicants</h2>
+    <div className="space-y-4">
+      <h2 className="text-xl">Task Applicants ({totalApplicants})</h2>
+
+      {!!applicants.length && applicants.map(applicant => (
+        <Card key={applicant.id as string}>
+          <CardHeader>
+            <h3 className="text-lg">
+              {(applicant.profiles as Record<string, unknown>).first_name as string}
+            </h3>
+          </CardHeader>
+          <CardContent>
+            <pre>{JSON.stringify(applicant, null, 2)}</pre>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   )
 }
