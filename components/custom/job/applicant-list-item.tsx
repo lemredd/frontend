c785de78522735acafd26e87fc3c84defd52e6d8
lemredd <lmoa@jhdp@gmail.com>
2024-10-05 15:17:37 +0800

    refactor(component): make applicant list item

---
+++ components/custom/job/applicant-list-item.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { MapPin } from "lucide-react"
+import { Chip } from "@/components/ui/chip"
+import { Button } from "@/components/ui/button"
+import { CollapsibleDesc } from "@/components/custom/collapsible-desc"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { useTransition } from "react"
+import { approveApplicant } from "@/actions/pdr/job"
+
+interface Props {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  applicant: Record<string, any>
+}
+
+export function ApplicantListItem({ applicant }: Props) {
+  const [isPending, startTransition] = useTransition()
+
+  function approve() {
+    startTransition(() => {
+      approveApplicant({ job_applicant_id: applicant.id }).then(({ error }) => {
+        if (error) console.error(error)
+      })
+    })
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <h3 className="text-lg font-bold">
+          {applicant.profiles.first_name as string}
+        </h3>
+        <p>{applicant.profiles.short_desc}</p>
+        <Chip
+          beforeContent={<MapPin size={16} />}
+          content={Object.values(applicant.profiles.addresses[0]).join(", ")}
+          className="w-max"
+          contentClassName="max-w-[unset]"
+        />
+      </CardHeader>
+      <CardContent>
+        <CollapsibleDesc content={applicant.proposal} />
+      </CardContent>
+      <CardFooter className="justify-end">
+        <Button disabled={isPending} onClick={approve}>Approve</Button>
+      </CardFooter>
+    </Card>
+  )
+}
