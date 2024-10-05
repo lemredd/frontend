4a3af98cfc851f3263438ab5584bf75e7d9509e4
lemredd <lmoa@jhdp@gmail.com>
2024-10-05 16:12:15 +0800

    feat(store): state `isJobOpen`

--- store/JobStore.ts
+++ store/JobStore.ts
@@ -4,12 +4,14 @@ interface JobState {
   job?: Record<string, unknown>
   isOwned: boolean
   setJob: (job: Record<string, unknown>, isOwned: boolean) => void
+  isJobOpen: () => boolean
 }
 
 export const useJobStore = create<JobState>()(
-  (set) => ({
+  (set, get) => ({
     job: undefined,
     isOwned: false,
-    setJob: (job, isOwned) => set({ job, isOwned })
+    setJob: (job, isOwned) => set({ job, isOwned }),
+    isJobOpen: () => get().job?.status === 'open',
   }),
 )
