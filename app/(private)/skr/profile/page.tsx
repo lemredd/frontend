687f459d9ba8ad27d40fc0131c857a0036d2f1a9
lemredd <lmoa@jhdp@gmail.com>
2024-10-06 13:48:57 +0800

    feat(page): refresh and display profile

--- app/(private)/skr/profile/page.tsx
+++ app/(private)/skr/profile/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 
+import { OwnProfileHeader } from "@/components/custom/profile/profile-header"
 import { useAuthStore } from "@/store/AuthStore"
+import { useEffect } from "react"
 
 export default function ProfilePage() {
-  const { user } = useAuthStore()
+  const { refreshUser } = useAuthStore()
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => refreshUser(), [])
 
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <OwnProfileHeader />
   )
 }
