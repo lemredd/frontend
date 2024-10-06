6eae6c9ed2907e3821c50a17f3ada1cfaed0c089
lemredd <lmoa@jhdp@gmail.com>
2024-10-06 14:52:02 +0800

    feat(component): add verification tooltip

--- components/custom/profile/verification-list.tsx
+++ components/custom/profile/verification-list.tsx
@@ -4,17 +4,20 @@ import { Phone, Mail } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useAuthStore } from "@/store/AuthStore"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 
 const VERIFICATIONS = [
   {
     name: "Phone",
     icon: Phone,
     check: (user: User | null) => !!user && !!user.phone && !!user.phone_confirmed_at,
+    get: (user: User | null) => user?.phone || "Unverified",
   },
   {
     name: "Email",
     icon: Mail,
     check: (user: User | null) => !!user && !!user.email && !!user.email_confirmed_at,
+    get: (user: User | null) => user?.email || "Unverified",
   },
 ]
 
@@ -30,9 +33,18 @@ export function VerificationList() {
         <ul className="flex gap-x-2 items-center">
           {VERIFICATIONS.map(verification => (
             <li key={verification.name as string}>
-              <Button variant="ghost" size="icon">
-                <verification.icon color={verification.check(user) ? "green" : "gray"} />
-              </Button>
+              <TooltipProvider delayDuration={0}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <verification.icon color={verification.check(user) ? "green" : "gray"} />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    {verification.get(user)}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </li>
           ))}
         </ul>
