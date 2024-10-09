d483959372ad3f59e00da3227f92978762a12dfa
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2024-10-10 02:38:32 +0800

    chore: update provider list of tasks design

--- app/(private)/pdr/tasks/page.tsx
+++ app/(private)/pdr/tasks/page.tsx
@@ -1,12 +1,12 @@
-import JobList from "@/components/custom/job/job-list"
-import { ProviderHeader as Header } from "@/components/custom/job/job-list-header"
+import JobList from '@/components/custom/job/job-list'
+import { ProviderHeader as Header } from '@/components/custom/job/job-list-header'
 
 export default function TaskListPage() {
   return (
-    <section className="space-y-4">
+    <>
       <Header />
 
       <JobList />
-    </section>
+    </>
   )
 }
--- components/custom/job/job-list-header.tsx
+++ components/custom/job/job-list-header.tsx
@@ -1,13 +1,13 @@
-"use client"
+'use client'
 
-import Link from "next/link"
-import { Plus, Search } from "lucide-react"
-import { useRouter, useSearchParams } from "next/navigation"
+import { Plus, Search } from 'lucide-react'
+import Link from 'next/link'
+import { useRouter, useSearchParams } from 'next/navigation'
 
-import { Button } from "@/components/ui/button"
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Button } from '@/components/ui/button'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
-const STATUSES = ["open", "ongoing", "completed", "closed"]
+const STATUSES = ['open', 'ongoing', 'completed', 'closed']
 
 export function ProviderHeader() {
   const router = useRouter()
@@ -19,34 +19,45 @@ export function ProviderHeader() {
 
     const newSearchParams = new URLSearchParams(searchParams.toString())
 
-    if (!value) newSearchParams.delete("search")
-    else newSearchParams.set("search", value)
+    if (!value) newSearchParams.delete('search')
+    else newSearchParams.set('search', value)
 
     router.push(`./?${newSearchParams.toString()}`)
   }
 
   function buildLink(status: string) {
     let to = `./?status=${status}`
-    if (searchParams.get("search")) to += `&search=${searchParams.get("search")}`
+    if (searchParams.get('search'))
+      to += `&search=${searchParams.get('search')}`
     return to
   }
 
   return (
     <>
-      <header className="flex items-center justify-between">
+      <header className="flex items-center justify-between w-full max-w-4xl mx-auto">
         <h2 className="text-2xl">Tasks</h2>
         <Button asChild>
-          <Link className="flex gap-x-2" href="post">
+          <Link
+            className="flex gap-x-2"
+            href="post"
+          >
             <Plus />
             Add Task
           </Link>
         </Button>
       </header>
-      <section className="space-y-4">
-        <Tabs defaultValue={searchParams.get("status") ?? "open"} className="w-[400px]">
+      <section className="space-y-4 w-full max-w-4xl mx-auto">
+        <Tabs
+          defaultValue={searchParams.get('status') ?? 'open'}
+          className="w-[400px]"
+        >
           <TabsList>
-            {STATUSES.map(status => (
-              <TabsTrigger key={status} value={status} asChild>
+            {STATUSES.map((status) => (
+              <TabsTrigger
+                key={status}
+                value={status}
+                asChild
+              >
                 <Link href={buildLink(status)}>
                   <span className="capitalize">{status}</span>
                 </Link>
@@ -54,13 +65,25 @@ export function ProviderHeader() {
             ))}
           </TabsList>
         </Tabs>
-        <form className="flex items-center w-full" onSubmit={search}>
+        <form
+          className="flex items-center w-full"
+          onSubmit={search}
+        >
           <div className="flex-grow flex items-center border border-r-0 rounded-l-md py-2 px-4 gap-x-2">
             <Search />
-            <input defaultValue={searchParams.get("search") ?? ""} name="search" className="focus:ring-0 focus:outline-none w-full bg-transparent" />
+            <input
+              defaultValue={searchParams.get('search') ?? ''}
+              name="search"
+              className="focus:ring-0 focus:outline-none w-full bg-transparent"
+            />
           </div>
           <div className="border border-primary rounded-r-md bg-blue-primary">
-            <Button size="lg" className=" border border-blue-primary">Search</Button>
+            <Button
+              size="lg"
+              className=" border border-blue-primary"
+            >
+              Search
+            </Button>
           </div>
         </form>
       </section>
