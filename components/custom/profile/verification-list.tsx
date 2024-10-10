f56ad06efee6674333399b6e51df6715539bc07e
lemredd <lmoa@jhdp@gmail.com>
2024-10-10 23:11:03 +0800

    feat(component): make seeker verification list

--- components/custom/profile/verification-list.tsx
+++ components/custom/profile/verification-list.tsx
@@ -10,6 +10,8 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip'
 import { useAuthStore } from '@/store/AuthStore'
+import { useEffect, useState } from 'react'
+import { getUserByUsername } from '@/actions/user'
 
 const VERIFICATIONS = [
   {
@@ -42,8 +44,8 @@ export function VerificationList() {
               <div className="flex items-center gap-x-3">
                 <verification.icon
                   className={`w-6 h-6 ${verification.check(user)
-                      ? 'text-green-500'
-                      : 'text-gray-400'
+                    ? 'text-green-500'
+                    : 'text-gray-400'
                     }`}
                 />
                 <span className="text-base font-medium">
@@ -57,8 +59,8 @@ export function VerificationList() {
                       variant="outline"
                       size="sm"
                       className={`${verification.check(user)
-                          ? 'border-green-500'
-                          : 'border-gray-400'
+                        ? 'border-green-500'
+                        : 'border-gray-400'
                         }`}
                     >
                       {verification.check(user) ? 'Verified' : 'Unverified'}
@@ -75,6 +77,61 @@ export function VerificationList() {
   )
 }
 
-export function SeekerVerificationList() {
+interface SeekerVerificationListProps {
+  username: string
+}
+export function SeekerVerificationList({ username }: SeekerVerificationListProps) {
+  const [user, setUser] = useState<User | null>(null)
 
+  console.log(user)
+  useEffect(() => {
+    getUserByUsername(username).then(data => setUser(data))
+  }, [username])
+
+  return (
+    <Card className="shadow-lg rounded-md">
+      <CardHeader className="pb-4">
+        <h3 className="text-lg font-semibold">Contacts</h3>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <ul className="space-y-4">
+          {VERIFICATIONS.map((verification) => (
+            <li
+              key={verification.name}
+              className="flex items-center justify-between gap-x-12"
+            >
+              <div className="flex items-center gap-x-3">
+                <verification.icon
+                  className={`w-6 h-6 ${verification.check(user)
+                    ? 'text-green-500'
+                    : 'text-gray-400'
+                    }`}
+                />
+                <span className="text-base font-medium">
+                  {verification.name}
+                </span>
+              </div>
+              <TooltipProvider delayDuration={0}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      className={`${verification.check(user)
+                        ? 'border-green-500'
+                        : 'border-gray-400'
+                        }`}
+                    >
+                      {verification.check(user) ? 'Verified' : 'Unverified'}
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>{verification.get(user)}</TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  )
 }
