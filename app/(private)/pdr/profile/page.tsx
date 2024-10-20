f79cea12d1d3ef03fd8df176aa3295a494abf6e0
lemredd <lmoa@jhdp@gmail.com>
2024-10-20 10:07:41 +0800

    feat(page): make own provider profile page

---
+++ app/(private)/pdr/profile/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { ProfileJobList } from '@/components/custom/job/job-list'
+import { OwnProfileHeader } from '@/components/custom/profile/profile-header'
+import Spinner from '@/components/custom/spinner'
+import { useAuthStore } from '@/store/AuthStore'
+import { useEffect } from 'react'
+
+export default function ProfilePage() {
+  const { refreshUser, isLoading } = useAuthStore()
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => refreshUser(), [])
+
+  if (isLoading) return <Spinner />
+
+  return (
+    <>
+      <OwnProfileHeader />
+      <ProfileJobList role="provider" />
+    </>
+  )
+
+}
+
