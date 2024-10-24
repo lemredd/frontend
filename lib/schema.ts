bdf0d4f08a6bc69b4be22bcfe2b89eb344c39644
lemredd <lmoa@jhdp@gmail.com>
2024-10-24 21:21:28 +0800

    fix(schema): transform skill id

--- lib/schema.ts
+++ lib/schema.ts
@@ -71,7 +71,10 @@ export const RegisterWithRoleSchema = z.object({
 })
 
 export const SkillsSchema = z.object({
-  skillIds: z.array(z.string()).min(1, { message: 'skill is required' }),
+  skillIds: z.array(z.string().transform(value => {
+    if (value.includes("|")) return value.split('|')[0]
+    return value
+  })).min(1, { message: 'skill is required' }),
 })
 
 export const ProfileDescriptionSchema = z.object({
