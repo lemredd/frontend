909f754635590b51de4b1ea76bd8fd7563aaa4e5
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2024-11-03 14:38:15 +0800

    chore: add missing pages

---
+++ app/(private)/(users)/pdr/page.tsx
@@ -0,0 +1,6 @@
+import { ProfileJobList } from '@/components/custom/job/job-list'
+
+// import Image from 'next/image'
+export default function Dashboard() {
+  return <ProfileJobList role="provider" />
+}
