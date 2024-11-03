fbda728f524fd4e2075095ba02441267a6443899
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2024-11-03 13:51:08 +0800

    chore: remove app

--- app/(public)/about/page.tsx
+++
@@ -1,4 +0,0 @@
-// import Image from 'next/image'
-export default function AboutPage() {
-  return <main className="h-screen w-full relative ">ABOUT</main>
-}
--- app/loading.tsx
+++
@@ -1,5 +0,0 @@
-import Spinner from '@/components/custom/spinner'
-
-export default function Loading() {
-  return <Spinner />
-}
