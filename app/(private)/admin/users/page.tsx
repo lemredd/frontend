44a2a416f9a27c5e9bdb4c42ed63fde143302f40
lemredd <lmoa@jhdp@gmail.com>
2024-11-04 21:42:34 +0800

    feat(page): use `UserTable`

--- app/(private)/admin/users/page.tsx
+++ app/(private)/admin/users/page.tsx
@@ -1,3 +1,4 @@
+import { UserTable } from "@/components/custom/admin/user-table"
 import { createAdminClient } from "@/utils/supabase/server"
 import { Suspense } from "react"
 
@@ -13,12 +14,14 @@ function Fallback() {
     </div>
   )
 }
+
 export default async function Users() {
   const supabase = createAdminClient()
-  const { data, error } = await supabase.auth.admin.listUsers({
+  const { data } = await supabase.auth.admin.listUsers({
     page: 1,
     perPage: 10
   })
+
   return (
     <Suspense fallback={<Fallback />}>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
@@ -27,8 +30,8 @@ export default async function Users() {
           <div className="aspect-video rounded-xl bg-muted/50" />
           <div className="aspect-video rounded-xl bg-muted/50" />
         </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-          {data?.users?.map((user) => <div key={user.id}>{user.id}</div>)}
+        <div className="min-h-[100vh] px-4 py-2 flex-1 rounded-xl bg-muted/50 md:min-h-min">
+          <UserTable users={data?.users} />
         </div>
       </div>
     </Suspense>
