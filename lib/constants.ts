b7ab12590ea19de7d0a446372bf48fbdb8d669a2
lemredd <lmoa@jhdp@gmail.com>
2024-11-09 10:16:02 +0800

    refactor(constant): add `USER_LIST_PAGE_SIZE`

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,6 @@
 export const PROFILE_STORE_FIELDS = '*, addresses(*), profile_skills(skills(*))'
 export const ITEMS_TO_DISPLAY = 3
 export const MAX_COLOR_VARIABLES = 10
+
+// List page sizes
+export const USER_LIST_PAGE_SIZE = 10
