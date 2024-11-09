5d104228cd752928cd7a44cae4e37df9d15993a3
lemredd <lmoa@jhdp@gmail.com>
2024-11-09 13:19:41 +0800

    feat(component): filter columns

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -1,13 +1,18 @@
 "use client"
 
+import { useState } from "react"
+
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   TableOptions,
   useReactTable,
 } from "@tanstack/react-table"
 
+import { Button } from "@/components/ui/button"
 import {
   Table,
   TableBody,
@@ -16,14 +21,13 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { Button } from "./button"
-import { useState } from "react"
-import { SelectedActionsProps } from "../custom/admin/user-table"
+import { TableInjectionProps } from "@/lib/types"
 
 interface DataTableProps<TData, TValue> extends Partial<TableOptions<TData>> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
-  SelectedActions?: (props: SelectedActionsProps<TData>) => React.ReactNode
+  SelectedActions?: (props: TableInjectionProps<TData>) => React.ReactNode,
+  SearchInput?: (props: TableInjectionProps<TData>) => React.ReactNode
 }
 
 export function DataTable<TData, TValue>({
@@ -33,91 +37,102 @@ export function DataTable<TData, TValue>({
   manualPagination,
   onPaginationChange,
   state,
-  SelectedActions
+  SelectedActions,
+  SearchInput
 }: DataTableProps<TData, TValue>) {
   const [rowSelection, setRowSelection] = useState({})
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
   const table = useReactTable({
     data,
     columns,
     pageCount,
     manualPagination,
     onPaginationChange,
-    state: { ...state, rowSelection },
+    state: { ...state, rowSelection, columnFilters },
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
     onRowSelectionChange: setRowSelection,
+    onColumnFiltersChange: setColumnFilters,
   })
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-      <div className="flex items-center justify-between px-2">
-        <div className="flex items-center justify-end space-x-2 py-4">
-          {!!table.getIsSomePageRowsSelected() && !!SelectedActions ? (
-            <SelectedActions table={table} />
-          ) : (
-            null
-          )}
+    <>
+      {!!SearchInput && (
+        <div className="flex items-center py-4">
+          <SearchInput table={table} />
         </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
+      )}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <div className="flex items-center justify-between px-2">
+          <div className="flex items-center justify-end space-x-2 py-4">
+            {!!table.getIsSomePageRowsSelected() && !!SelectedActions ? (
+              <SelectedActions table={table} />
+            ) : (
+              null
+            )}
+          </div>
+          <div className="flex items-center justify-end space-x-2 py-4">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
