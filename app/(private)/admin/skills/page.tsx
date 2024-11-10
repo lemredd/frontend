fe24bf2441c9c4b11eb278406ab597b36dd4fe66
lemredd <lmoa@jhdp@gmail.com>
2024-11-10 14:47:00 +0800

    feat(page): hide sections

--- app/(private)/admin/skills/page.tsx
+++ app/(private)/admin/skills/page.tsx
@@ -7,11 +7,13 @@ import { SkillsTable } from "@/components/custom/admin/skills-table"
 function Fallback() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      {/*
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
       </div>
+      */}
       <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
     </div>
   )
@@ -37,11 +39,13 @@ export default async function Skills({ searchParams }: SkillsProps) {
   return (
     <Suspense fallback={<Fallback />}>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
+        {/*
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+      </div>
+      */}
         <div className="min-h-[100vh] px-4 py-2 flex-1 rounded-xl bg-muted/50 md:min-h-min">
           <SkillsTable data={result} />
         </div>
--- app/(private)/admin/users/page.tsx
+++ app/(private)/admin/users/page.tsx
@@ -6,11 +6,13 @@ import { UserTable } from "@/components/custom/admin/user-table"
 function Fallback() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      {/*
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
       </div>
+      */}
       <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
     </div>
   )
@@ -26,11 +28,13 @@ export default async function Users({ searchParams }: UsersProps) {
   return (
     <Suspense fallback={<Fallback />}>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
+        {/*
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+      </div>
+      */}
         <div className="min-h-[100vh] px-4 py-2 flex-1 rounded-xl bg-muted/50 md:min-h-min">
           <UserTable data={data} />
         </div>
