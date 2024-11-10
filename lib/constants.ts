ee078d5fb9d229e552e669db754a593a1d9318f4
lemredd <lmoa@jhdp@gmail.com>
2024-11-10 14:46:49 +0800

    feat(constant): adjust list table limit

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,6 @@ export const ITEMS_TO_DISPLAY = 3
 export const MAX_COLOR_VARIABLES = 10
 
 // List page sizes
-export const USER_LIST_PAGE_SIZE = 10
-export const SKILL_LIST_PAGE_SIZE = 10
-export const JOB_LIST_PAGE_SIZE = 10
+export const USER_LIST_PAGE_SIZE = 15
+export const SKILL_LIST_PAGE_SIZE = 15
+export const JOB_LIST_PAGE_SIZE = 15
