1989fa5af3bca647748bb06755fccda9bb4f86ee
lemredd <lmoa@jhdp@gmail.com>
2024-11-12 21:01:25 +0800

    feat(page): filter seeker applied jobs

--- app/(private)/(users)/skr/page.tsx
+++ app/(private)/(users)/skr/page.tsx
@@ -1,6 +1,12 @@
 import { ProfileJobList } from '@/components/custom/job/job-list'
+import { SeekerHeader } from '@/components/custom/job/job-list-header'
 
 // import Image from 'next/image'
 export default function Dashboard() {
-  return <ProfileJobList role="seeker" />
+  return (
+    <>
+      <SeekerHeader />
+      <ProfileJobList role="seeker" />
+    </>
+  )
 }
