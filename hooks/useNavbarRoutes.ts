a00fdd50725d22af34716e9c2f499e2f140c42c8
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2024-11-15 13:29:28 +0800

    fix(hook): hide navbar route

--- hooks/useNavbarRoutes.ts
+++ hooks/useNavbarRoutes.ts
@@ -1,38 +1,38 @@
-import { TNavbarRoute, UserRole } from '@/lib/types'
-import { useMemo } from 'react'
+import { TNavbarRoute, UserRole } from "@/lib/types";
+import { useMemo } from "react";
 
 const useNavbarRoutes = (role?: UserRole): TNavbarRoute[] => {
   const NavbarRoutes: TNavbarRoute[] = useMemo(() => {
     // Routes for unauthenticated users
     const publicRoutes: TNavbarRoute[] = [
-      { label: 'Home', path: '/#' },
-      { label: 'About', path: '/#about' },
-      { label: 'Contact Us', path: '/#contact-us' },
-    ]
+      { label: "Home", path: "/#" },
+      { label: "About", path: "/#about" },
+      // { label: 'Contact Us', path: '/#contact-us' },
+    ];
 
     // Routes for SKRs
     const seekerRoutes: TNavbarRoute[] = [
-      { label: 'Home', path: '/skr/' },
-      { label: 'Grab Tasks', path: '/skr/tasks/' },
-    ]
+      { label: "Home", path: "/skr/" },
+      { label: "Grab Tasks", path: "/skr/tasks/" },
+    ];
 
     // Routes for PDRs
     const providerRoutes: TNavbarRoute[] = [
-      { label: 'Home', path: '/pdr/' },
-      { label: 'Tasks', path: '/pdr/tasks/' },
-    ]
+      { label: "Home", path: "/pdr/" },
+      { label: "Tasks", path: "/pdr/tasks/" },
+    ];
 
     // Return different routes based on the role
-    if (role === 'SKR') {
-      return seekerRoutes
-    } else if (role === 'PDR') {
-      return providerRoutes
+    if (role === "SKR") {
+      return seekerRoutes;
+    } else if (role === "PDR") {
+      return providerRoutes;
     } else {
-      return publicRoutes // For unauthenticated users
+      return publicRoutes; // For unauthenticated users
     }
-  }, [role])
+  }, [role]);
 
-  return NavbarRoutes
-}
+  return NavbarRoutes;
+};
 
-export default useNavbarRoutes
+export default useNavbarRoutes;
