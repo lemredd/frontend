a6aaa8896982d0cc38ce23fffe17b6e67b629114
Lem Redd <lmoa.jhdp@gmail.com>
2024-11-29 20:59:59 +0800

    chore(constant): store profile seeker approval

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const PROFILE_STORE_FIELDS = '*, addresses(*), profile_skills(skills(*))'
+export const PROFILE_STORE_FIELDS = '*, addresses(*), profile_skills(skills(*)), approvals(*)'
 export const ITEMS_TO_DISPLAY = 3
 export const MAX_COLOR_VARIABLES = 10
 
