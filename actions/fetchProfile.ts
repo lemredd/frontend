01099a7d72917587361d4daf648b5cc0ce316821
Lem Redd <lmoa.jhdp@gmail.com>
2024-11-30 10:04:00 +0800

    fix(action): refresh profile properly

--- actions/fetchProfile.ts
+++ actions/fetchProfile.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { PROFILE_STORE_FIELDS } from '@/lib/constants'
 import { formatErrorMessage } from '@/lib/utils'
 import { createClient } from '@/utils/supabase/server'
 
@@ -8,7 +9,7 @@ export const fetchProfile = async (user_id: string) => {
 
   const { error, data: profile } = await supabase
     .from('profiles')
-    .select('*')
+    .select(PROFILE_STORE_FIELDS)
     .eq('user_id', user_id)
     .single()
 
